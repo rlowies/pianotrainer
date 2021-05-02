dedcb9cb954fe4e95717ab6455d59bf27781ac92
Ron Lowies <ronlowies@u.boisestate.edu>
2021-05-01 22:52:09 -0600

    Converted to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
-import WebMidi from 'webmidi';
-import React, { useEffect, useState } from 'react';
+import WebMidi, { InputEventNoteon } from 'webmidi';
+import { useEffect, useState } from 'react';
 
 function App() {
   // const [isMidiEnabled, setIsMidiEnabled] = useState(false);
   // const [input, setInput] = useState();
+  const [note, setNote] = useState<string>();
   useEffect(() => {
     WebMidi.enable(function (err) {
 
@@ -13,9 +14,14 @@ function App() {
       } else {
         // setIsMidiEnabled(true);
         var midiInput = WebMidi.getInputByName("Roland Digital Piano");
-        midiInput.addListener('noteon', "all", function (e) {
-          console.log("Received 'noteon' message (" + e.note.name + e.note.octave + ").")
-        });
+        if(midiInput){
+          
+          midiInput.addListener('noteon', "all", function (e: InputEventNoteon) {
+            var noteValue: string = e.note.name + e.note.octave; 
+            console.log(`Received: "${noteValue}"`)
+            setNote(noteValue);
+          });
+        }
         console.log("WebMidi enabled!", WebMidi.inputs);
         // setInput(midiInput);
       }
@@ -28,6 +34,7 @@ function App() {
   return (
     <>
       <div className="App">
+        {note}
       </div>
     </>
   );
