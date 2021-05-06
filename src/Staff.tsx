e257b85ecdbba01f7f1ab2e4e7aeb6d57c173eef
Ron Lowies <ronlowies@u.boisestate.edu>
2021-05-05 23:49:57 -0600

    Added more notes and a toggle between clefs

--- src/Staff.tsx
+++ src/Staff.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import Vex from 'vexflow';
-import { bassClefEasy, generateNotes, INote, randomSort } from './services/Note.service';
+import { bassClefEasy, bassClefMedium, generateNotes, INote, randomSort, trebleClefMedium } from './services/Note.service';
 
 interface StaffConfig {
     VF: typeof Vex.Flow;
@@ -19,12 +19,33 @@ const initialStaffConfig: StaffConfig = {
 export default function Staff(props: any) {
     const [note, setNote] = useState<string>("");
     const [init, setInit] = useState<boolean>(false);
-    const [playableNotes, setPlayableNotes] = useState<INote[]>(generateNotes(bassClefEasy, true, 4));
     const [staffConfig, setStaffConfig] = useState<StaffConfig>(initialStaffConfig);
+    const [clefType, setStaffType] = useState<string>("bass");
+    const [playableNotes, setPlayableNotes] = useState<INote[]>(generateNotes(bassClefEasy, true, 4, clefType));
     const [notes, setNotes] = useState<Vex.Flow.StaveNote[]>(playableNotes.map(x => x.note));
 
     useEffect(() => {
         var { VF, staff, currentNoteIndex, numNotes } = staffConfig;
+
+        const initVoice = () => {
+            var voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
+            voice.addTickables(notes);
+            // Format and justify the notes to 400 pixels.
+            new VF.Formatter().joinVoices([voice]).format([voice], 400);
+            voice.draw(staff.getContext(), staff);
+        }
+
+        const resetStaff = (type: string, config: StaffConfig) => {
+            var newNotes = generateNotes(type === "bass" ? bassClefMedium : trebleClefMedium, true, 4, clefType);
+            setPlayableNotes(newNotes);
+            setNotes(newNotes.map(x => x.note));
+            config.staff = new Vex.Flow.Stave(10, 40, 400);
+            staff.getContext().clear();
+            config.staff.addClef(type).addTimeSignature("4/4");
+            config.staff.setContext(staff.getContext()).draw();
+            config.currentNoteIndex = 0;
+            setStaffConfig(config)
+        }
 
         const updateStaff = () => {
             var red = { fillStyle: "#cc0000", strokeStyle: "#cc0000" };
@@ -35,14 +56,7 @@ export default function Staff(props: any) {
                     ...staffConfig
                 }
                 if (note === "C8") {
-                    var newNotes = generateNotes(bassClefEasy, true, 4);
-                    staff.getContext().clear();
-                    staff.setContext(staff.getContext()).draw();
-
-                    setPlayableNotes(newNotes);
-                    setNotes(newNotes.map(x => x.note));
-                    staffConfigUpdate.currentNoteIndex = 0;
-                    setStaffConfig(staffConfigUpdate)
+                    resetStaff(clefType, staffConfigUpdate)
                     return;
                 }
 
@@ -59,11 +73,7 @@ export default function Staff(props: any) {
                 setStaffConfig(staffConfigUpdate)
             }
 
-            var voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
-            voice.addTickables(notes);
-            // Format and justify the notes to 400 pixels.
-            new VF.Formatter().joinVoices([voice]).format([voice], 400);
-            voice.draw(staff.getContext(), staff);
+            initVoice();
         }
 
         const Initialize = () => {
@@ -71,7 +81,7 @@ export default function Staff(props: any) {
             var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
             renderer.resize(500, 500);
             var context = renderer.getContext();
-            staff.addClef("bass").addTimeSignature("4/4");
+            staff.addClef(clefType).addTimeSignature("4/4");
             staff.setContext(context).draw();
             updateStaff();
             setInit(true);
@@ -82,14 +92,23 @@ export default function Staff(props: any) {
         }
         setNote(props.note);
         updateStaff();
-    }, [props.note, playableNotes, note, init, staffConfig])
-
+    }, [props.note, playableNotes, note, init, staffConfig, clefType, notes])
 
     return (
         <>
             <div id='staff' />
             {playableNotes.map((x: INote, i: number) => <button key={i} onClick={() => setNote(x.name)}> Send {x.name}</button>).sort(randomSort)}
             <button onClick={() => { setNote("C8") }}> Reset</button>
+            <button onClick={() => {
+                if (clefType === "bass") {
+                    setStaffType("treble");
+                }
+                else {
+                    setStaffType("bass");
+                }
+                setNote("C8");
+            }
+            }> Change Staff</button>
         </>
     );
 
--- src/services/Note.service.ts
+++ src/services/Note.service.ts
@@ -6,7 +6,7 @@ export interface INote {
     order: number;
 }
 
-export const generateNotes = (notes: string, random: boolean = false, count: number): INote[] => {
+export const generateNotes = (notes: string, random: boolean = false, count: number, clef: string): INote[] => {
     var res: INote[] = [];
     var allNotes = notes.split(",");
     const VF = Vex.Flow;
@@ -18,11 +18,9 @@ export const generateNotes = (notes: string, random: boolean = false, count: num
     for (var i = 0; i < count; i++) {
         var currentKey = allNotes[i];
 
-        var noteName = currentKey.replace("/", "").toUpperCase();
-
         res.push({
-            note: new VF.StaveNote({ clef: "bass", keys: [currentKey], duration: "q" }),
-            name: noteName,
+            note: new VF.StaveNote({ clef: clef, keys: [currentKey], duration: "q", auto_stem: true }),
+            name: currentKey.replace("/", "").toUpperCase(),
             order: i
         });
     }
@@ -31,6 +29,18 @@ export const generateNotes = (notes: string, random: boolean = false, count: num
 }
 
 export const randomSort = () => 0.5 - Math.random();
-// const bassClefInSpaces:string = "a/2,b/1,b/3,c/1,c/3,d/2,e/1,e/3,g/1,g/3";
-// const bassClefInLines:string = "a/1,a/3,c/2,c/4,b/2,d/1,d/3,e/2,f/1,g/2";
+const bassClefInSpaces:string = "a/2,b/1,b/3,c/1,c/3,d/2,e/1,e/3,g/1,g/3";
+const bassClefInLines:string = "a/1,a/3,c/2,c/4,b/2,d/1,d/3,e/2,f/1,g/2";
+
+export const bassCleffHard:string = bassClefInLines + "," + bassClefInSpaces;
 export const bassClefEasy:string = "a/2,c/3,e/3,g/3,g/2,b/2,d/3,f/3,a/3";
+
+export const trebleClefInLines:string = "a/3,c/4,e/4,g/4,b/4,d/5,f/5,a/5,c/6";//2 above and below ledger 
+export const trebleClefInSpaces:string = "b/3,d/4,f/4,a/4,c/5,e/5,g/5,b/5";//2 above and below ledger 
+export const trebleClefEasy = "e/4,g/4,b/4,d/5,f/5,f/4,a/4,c/5,e/5";
+export const trebleClefMedium = trebleClefInLines + "," + trebleClefInSpaces;
+
+export const bassClefMedium:string = "c/2,d/2,e/2,f/2,g/2,a/2,b/2,c/3,d/3,e/3,f/3,g/3,a/3,b/3,c/4";
+
+// cdefgab2
+// cdefgab3
